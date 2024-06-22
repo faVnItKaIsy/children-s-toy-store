733f0e07685306396a8cf67dc1ddbeba5c568252
faVnItKaIsy <Fantasy821@mail.ru>
2024-06-22 09:04:12 +0700

    1

---
+++ script.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+
+/*let activeSlide = 0;
+setInterval(() => {
+ slides[slideIndex].classList.remove('active');
+ slideIndex++;
+ if (slideIndex == slides.length) {
+  slideIndex = 0; 
+ }
+ slides[slideIndex].classList.add('active'); 
+
+}, 2000); //через каждые 2 секунды слайд будет меняться
+*/
+
+
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
